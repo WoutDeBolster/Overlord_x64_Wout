64dbbfc6d164661b674e6739b48030c2ad6fd82c
nedelpedel <wout.de.bolster@student.howest.be>
2022-04-14 17:56:43 +0200

    adding the pickier and the sprite font loader

--- OverlordEngine/Components/CameraComponent.cpp
+++ OverlordEngine/Components/CameraComponent.cpp
@@ -60,11 +60,45 @@ void CameraComponent::SetActive(bool active)
 	ASSERT_IF(!pScene, L"Failed to set active camera. Parent game scene is null");
 
 	m_IsActive = active;
-	pScene->SetActiveCamera(active?this:nullptr); //Switch to default camera if active==false
+	pScene->SetActiveCamera(active ? this : nullptr); //Switch to default camera if active==false
 }
 
-GameObject* CameraComponent::Pick(CollisionGroup /*ignoreGroups*/) const
+GameObject* CameraComponent::Pick(CollisionGroup ignoreGroups) const
 {
-	TODO_W5(L"Implement Picking Logic")
+	//TODO_W5(L"Implement Picking Logic")
+	auto sceneContext{ m_pScene->GetSceneContext() };
+	auto mousePos{ sceneContext.pInput->GetMousePosition() };
+	const float viewWidth{ (m_Size > 0) ? m_Size * sceneContext.aspectRatio : sceneContext.windowWidth };
+	const float viewHeight = { (m_Size > 0) ? m_Size : sceneContext.windowHeight };
+	float halfViewWidth{ viewWidth / 2.f };
+	float halfViewHeight{ viewWidth / 2.f };
+
+	// step 1.
+	float Xndc{ (mousePos.x - halfViewWidth) / halfViewWidth };
+	float Yndc{ (halfViewHeight - mousePos.y) / halfViewHeight };
+
+	// step 2.
+	XMMATRIX ViewProjection{ XMLoadFloat4x4(&m_ViewProjection) };
+	XMMATRIX ViewProjectionInverted{ XMMatrixInverse(nullptr, ViewProjection) };
+
+	XMVECTOR nearPoint{ XMVector4Transform(XMVECTOR(Xndc, Yndc, 0, 0),ViewProjectionInverted) };
+	XMVECTOR farPoint{ XMVector4Transform(XMVECTOR(Xndc, Yndc, 1, 0),ViewProjectionInverted) };
+
+	XMFLOAT4 startFloat{};
+	XMFLOAT4 DirectionFloat{};
+	XMStoreFloat4(&startFloat, nearPoint);
+	XMStoreFloat4(&DirectionFloat, farPoint);
+	PxVec3 rayStart{ startFloat.x, startFloat.y, startFloat.z };
+	PxVec3 raydirection{ DirectionFloat.x, DirectionFloat.y, DirectionFloat.z };
+
+	// step  3.
+	PxQueryFilterData filterData{};
+	filterData.data.word0 = ~UINT(ignoreGroups);
+
+	PxRaycastBuffer hit{};
+	if (m_pScene->GetPhysxProxy()->Raycast(rayStart, raydirection, PX_MAX_F32, hit, PxHitFlag::eDEFAULT, filterData))
+	{
+		//return hit.getAnyHit(0);
+	}
 	return nullptr;
 }
--- OverlordEngine/Content/SpriteFontLoader.cpp
+++ OverlordEngine/Content/SpriteFontLoader.cpp
@@ -38,7 +38,7 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//If version is < 3,
 	//Log Error (SpriteFontLoader::LoadContent > Only .fnt version 3 is supported)
 	//return nullptr
-	auto version = pReader->Read<char>();
+	char version = pReader->Read<char>();
 	if (version != '3')
 	{
 		Logger::LogError(L"SpriteFontLoader::LoadContent > Only .fnt version 3 is supported");
@@ -49,6 +49,7 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//use this SpriteFontDesc to store all relevant information (used to initialize a SpriteFont object)
 	SpriteFontDesc fontDesc{};
 
+	// see https://www.angelcode.com/products/bmfont/doc/file_format.html#bin for magic numbers and all the other things
 	//**********
 	// BLOCK 0 *
 	//**********
@@ -57,6 +58,15 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//Move the binreader to the start of the FontName [BinaryReader::MoveBufferPosition(...) or you can set its position using BinaryReader::SetBufferPosition(...))
 	//Retrieve the FontName [fontDesc.fontName]
 	//...
+	char blockId{ pReader->Read<char>() };
+	int blockSize{ pReader->Read<int>() };
+	int fontSize{ pReader->Read<int>() };
+	pReader->MoveBufferPosition(12);
+	std::wstring fontName{ pReader->ReadNullString() };
+
+	fontDesc.fontSize = short(fontSize);
+	fontDesc.fontName = fontName;
+
 
 	//**********
 	// BLOCK 1 *
@@ -68,6 +78,20 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//	> Log Error (Only one texture per font is allowed!)
 	//Advance to Block2 (Move Reader)
 	//...
+	blockId = pReader->Read<char>();
+	blockSize = pReader->Read<int>();
+	pReader->MoveBufferPosition(2);
+	unsigned int lingeHeight{ pReader->Read<unsigned int>() };
+	unsigned int lingeWithe{ pReader->Read<unsigned int>() };
+	unsigned int pageCount{ pReader->Read<unsigned int>() };
+	if (pageCount > 1)
+	{
+		Logger::LogError(L"Only one texture per font is allowed!");
+	}
+	pReader->MoveBufferPosition(5);
+
+	fontDesc.textureHeight = short(lingeHeight);
+	fontDesc.textureWidth = short(lingeWithe);
 
 	//**********
 	// BLOCK 2 *
@@ -78,6 +102,12 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//	>> page texture should be stored next to the .fnt file, pageName contains the name of the texture file
 	//	>> full texture path = asset parent_path of .fnt file (see loadInfo.assetFullPath > get parent_path) + pageName (filesystem::path::append)
 	//	>> Load the texture (ContentManager::Load<TextureData>) & Store [fontDesc.pTexture]
+	blockId = pReader->Read<char>();
+	blockSize = pReader->Read<int>();
+	std::wstring pageName{ pReader->ReadNullString() };
+	std::wstring fullPath{ loadInfo.assetFullPath.parent_path().append(pageName) };
+	fontDesc.pTexture = ContentManager::Load<TextureData>(fullPath);
+
 
 	//**********
 	// BLOCK 3 *
@@ -104,6 +134,29 @@ SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	//	> key = (wchar_t) charId
 	//	> value = new FontMetric
 	//(loop restarts till all metrics are parsed)
+	blockId = pReader->Read<char>();
+	blockSize = pReader->Read<int>();
+	unsigned int numChars = { static_cast<unsigned int>(blockSize / 20) };
+
+	for (unsigned int i = 0; i < numChars; i++)
+	{
+		unsigned int id{ pReader->Read<unsigned int>() };
+		FontMetric metric;
+		metric.character = wchar_t(id);
+		unsigned int posX{ pReader->Read<unsigned short>() };
+		unsigned int posY{ pReader->Read<unsigned short>() };
+		metric.width = pReader->Read<unsigned short>();
+		metric.height = pReader->Read<unsigned short>();
+		metric.offsetX = pReader->Read<short>();
+		metric.offsetY = pReader->Read<short>();
+		metric.advanceX = pReader->Read<short>();
+		metric.page = pReader->Read<unsigned char>();
+		metric.channel = pReader->Read<unsigned char>();
+		metric.texCoord = { posX / (float)metric.width, posY / (float)metric.height };
+
+		wchar_t charId{ wchar_t(id) };
+		fontDesc.metrics.insert(std::make_pair(charId, metric));
+	}
 
 	//Done!
 	delete pReader;
